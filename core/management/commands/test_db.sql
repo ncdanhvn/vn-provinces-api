e01ecf84df35375ee6753a599938843cca4d7828
Danh <ncdanhvn@gmail.com>
2023-05-08 10:42:57 +0700

    Completing all tests

--- core/management/commands/test_db.sql
+++ core/management/commands/test_db.sql
@@ -7,3 +7,24 @@ values  (1, 'Tây Bắc Bộ', 'Northwest'),
         (6, 'Tây Nguyên', 'Central Highlands'),
         (7, 'Đông Nam Bộ', 'Southeast'),
         (8, 'Đồng bằng sông Cửu Long', 'Mekong River Delta');
+insert into core_province (id, name, region_id, type)
+values  (1, 'Hà Nội', 3, 'C'),
+        (2, 'Hà Giang', 2, 'P'),
+        (4, 'Cao Bằng', 2, 'P'),
+        (6, 'Bắc Kạn', 2, 'P'),
+        (30, 'Hải Dương', 3, 'P'),
+        (10, 'Lào Cai', 1, 'P');
+insert into core_district (id, name, type, province_id)
+values  (1, 'Ba Đình', 'UD', 1),
+        (2, 'Hoàn Kiếm', 'UD', 1),
+        (3, 'Tây Hồ', 'UD', 1),
+        (24, 'Hà Giang', 'C', 2),
+        (26, 'Đồng Văn', 'RD', 2),
+        (27, 'Mèo Vạc', 'RD', 2),   
+        (64, 'Chợ Đồn', 'RD', 6),
+        (65, 'Chợ Mới', 'RD', 6),     
+        (86, 'Bảo Thắng', 'RD', 10),
+        (87, 'Bảo Yên', 'RD', 10);                
+insert into core_ward (id, name, type, district_id)
+values  (1, 'Phúc Xá', 'W', 1),
+        (4, 'Trúc Bạch', 'W', 1);
--- core/tests/test_regions.py
+++ core/tests/test_regions.py
@@ -5,9 +5,72 @@ import pytest
 
 @pytest.mark.django_db
 class TestRegions:
-    def test_if_list_regions_return_200(self):
+    def test_if_list_regions_return_200_and_correct_regions_count(self):
         client = APIClient()
         response = client.get('/api/regions/')
         
         assert response.status_code == status.HTTP_200_OK
         assert len(response.data) == 8  # There are 8 regions in test db
+
+    def test_if_retrieve_region_return_200_and_correct_content(self):
+        client = APIClient()
+        response = client.get('/api/regions/3/')
+        
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['name_en'] == 'Red River Delta'    
+        assert response.data['provinces_count'] == 2    # This region has 2 provinces
+
+
+@pytest.mark.django_db
+class TestProvinces:
+    def test_if_list_provinces_return_200_and_correct_provinces_count(self):
+        client = APIClient()
+        response = client.get('/api/provinces/')
+        
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['count'] == 6  # There are 6 provinces in test db
+
+    def test_if_retrieve_province_return_200_and_correct_content(self):
+        client = APIClient()
+        response = client.get('/api/provinces/1/')
+        
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['name_en'] == 'Ha Noi'
+        assert len(response.data['districts']) == 3    # Ha Noi has 3 districts in test db 
+        assert response.data['wards_count'] == 2    # Ha Noi has 2 wards in test db 
+
+
+@pytest.mark.django_db
+class TestDistricts:
+    def test_if_list_districts_return_200_and_correct_districts_count(self):
+        client = APIClient()
+        response = client.get('/api/districts/')
+        
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['count'] == 10  # There are 10 districts in test db
+
+    def test_if_retrieve_district_return_200_and_correct_content(self):
+        client = APIClient()
+        response = client.get('/api/districts/1/')
+        
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['name_en'] == 'Ba Dinh'    
+        assert len(response.data['wards']) == 2    # This district has 0 wards in test db 
+        
+
+@pytest.mark.django_db
+class TestWards:
+    def test_if_list_wards_return_200_and_correct_wards_count(self):
+        client = APIClient()
+        response = client.get('/api/wards/')
+        
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['count'] == 2  # There are 2 wards in test db
+
+    def test_if_retrieve_ward_return_200_and_correct_content(self):
+        client = APIClient()
+        response = client.get('/api/wards/1/')
+        
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['name_en'] == 'Phuc Xa'    
+        assert response.data['province']['id'] == 1   
