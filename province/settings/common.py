a1545c87fd40510f3abb2accb9daff92ec521de0
Danh Nguyen <ncdanhvn@gmail.com>
2023-09-16 10:01:22 +0700

    Add static

--- province/settings/common.py
+++ province/settings/common.py
@@ -108,6 +108,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
